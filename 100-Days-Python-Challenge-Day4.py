cf4beeb15df5b488d60065f9d326aad0a9552b49
Aayushi-Modi <modiaayushi@gmail.com>
2022-04-22 23:04:42 +0530

    [AM]-Day4

--- 100-Days-Python-Challenge-Day4.py
+++ 100-Days-Python-Challenge-Day4.py
@@ -1,5 +1,34 @@
-vowels = ['a','e','i','o','u']
-str1 = 'Aayushi Modi'
-for i in str1:
-    if i == vowels[i]:
-        print(i)
+#!/usr/bin/env python
+# coding: utf-8
+
+# "Aayushi Modi", remove all the vowels from this string and return the residual string.
+
+# In[55]:
+
+
+vowel = ['a','e','i','o','u']
+str1 = 'Python Programming'
+str2 = str1.lower()
+for i in str2:
+    if i in vowel:
+        continue
+    else:
+        print(i, end="")
+
+
+# "hahlolaayuhalolhhellohehewtflol",  find the starting index of first occurance of "aayushi" in that string.
+
+# In[56]:
+
+
+str1 = "hahlolaayuhalolhhellohehewtflol"
+str2 = 'hello'
+if str2 in str1:
+    print(str1.index(str2))
+
+
+# In[ ]:
+
+
+
+

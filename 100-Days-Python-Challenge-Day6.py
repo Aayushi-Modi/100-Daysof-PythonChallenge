0e70f9f88dbfc52b743e35cb5d41d1df8e3d60e6
Aayushi-Modi <modiaayushi@gmail.com>
2022-04-24 20:27:49 +0530

    [AM]-Day6

---
+++ 100-Days-Python-Challenge-Day6.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+str1 = "Aayushi Modi is the best coder in the world"
+str2 = str1.lower()
+for i in str2:
+    str2.split(',')
+    print('Frequency of ', i ,' is:', str2.count(i))
+
+
+# In[ ]:
+
+
+
+
